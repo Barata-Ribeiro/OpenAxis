a0aff171c30144e723330fd92895c560c374d2c1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-13 09:20:54 -0300

    feat(app-header): replace AppLogo with AppLogoSvg component

--- resources/js/components/application/app-header.tsx
+++ resources/js/components/application/app-header.tsx
@@ -18,8 +18,8 @@ import { dashboard } from '@/routes';
 import { type BreadcrumbItem, type NavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-react';
-import AppLogo from './app-logo';
 import AppLogoIcon from './app-logo-icon';
+import AppLogoSvg from './app-logo-svg';
 
 const mainNavItems: NavItem[] = [
     {
@@ -109,7 +109,7 @@ export function AppHeader({ breadcrumbs = [] }: Readonly<AppHeaderProps>) {
                     </div>
 
                     <Link href={dashboard()} prefetch className="flex items-center space-x-2">
-                        <AppLogo />
+                        <AppLogoSvg className="h-8 w-full" />
                     </Link>
 
                     {/* Desktop Navigation */}
