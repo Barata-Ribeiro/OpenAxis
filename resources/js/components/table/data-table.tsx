e3da92b7956c5035b55cdceb40c46ea9c4731ba8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-17 10:12:38 -0300

    feat(pagination): add DataTablePagination component for enhanced pagination controls

--- resources/js/components/table/data-table.tsx
+++ resources/js/components/table/data-table.tsx
@@ -1,15 +1,48 @@
+'use no memo';
+
 import { ColumnDef, flexRender, getCoreRowModel, SortingState, useReactTable } from '@tanstack/react-table';
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { buildParams } from '@/lib/utils';
 import { PaginationMeta } from '@/types';
 import { router } from '@inertiajs/react';
+import type { Column } from '@tanstack/react-table';
 import { useLayoutEffect, useState } from 'react';
+import { DataTablePagination } from './data-table-pagination';
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
     data: PaginationMeta<TData[]>['data'];
     pagination: Omit<PaginationMeta<TData[]>, 'data'>;
+}
+
+function getCommonPinningStyles<TData>({
+    column,
+    withBorder = false,
+}: {
+    column: Column<TData>;
+    withBorder?: boolean;
+}): React.CSSProperties {
+    const isPinned = column.getIsPinned();
+    const isLastLeftPinnedColumn = isPinned === 'left' && column.getIsLastColumn('left');
+    const isFirstRightPinnedColumn = isPinned === 'right' && column.getIsFirstColumn('right');
+
+    const leftPinnedBoxShadow = '-4px 0 4px -4px var(--border) inset';
+    const rightPinnedBoxShadow = '4px 0 4px -4px var(--border) inset';
+
+    const rightPinnedShadow = isFirstRightPinnedColumn ? rightPinnedBoxShadow : undefined;
+    const pinnedColumnShadow = isLastLeftPinnedColumn ? leftPinnedBoxShadow : rightPinnedShadow;
+
+    return {
+        boxShadow: withBorder ? pinnedColumnShadow : undefined,
+        left: isPinned === 'left' ? `${column.getStart('left')}px` : undefined,
+        right: isPinned === 'right' ? `${column.getAfter('right')}px` : undefined,
+        opacity: isPinned ? 0.97 : 1,
+        position: isPinned ? 'sticky' : 'relative',
+        background: 'var(--background)',
+        width: column.getSize(),
+        zIndex: isPinned ? 1 : undefined,
+    };
 }
 
 export function DataTable<TData, TValue>({ columns, data, pagination }: Readonly<DataTableProps<TData, TValue>>) {
@@ -34,10 +67,9 @@ export function DataTable<TData, TValue>({ columns, data, pagination }: Readonly
                 pageIndex: Math.max((pagination.current_page ?? 1) - 1, 0),
                 pageSize: pagination.per_page,
             },
+            columnPinning: { left: ['id'], right: ['actions'] },
         },
-        state: {
-            sorting,
-        },
+        state: { sorting },
         onSortingChange: setSorting,
     });
 
@@ -64,46 +96,59 @@ export function DataTable<TData, TValue>({ columns, data, pagination }: Readonly
     }, [sorting, pagination.path]);
 
     return (
-        <div className="overflow-hidden rounded-md border">
-            {/* TODO: Add search and date filters */}
-            <Table>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(header.column.columnDef.header, header.getContext())}
-                                    </TableHead>
-                                );
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody className="bg-">
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
+        <div className="mx-auto w-full flex-col space-y-4">
+            <div className="overflow-hidden rounded-md border">
+                {/* TODO: Add search and date filters */}
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead
+                                            key={header.id}
+                                            colSpan={header.colSpan}
+                                            style={{
+                                                ...getCommonPinningStyles({ column: header.column }),
+                                            }}
+                                        >
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(header.column.columnDef.header, header.getContext())}
+                                        </TableHead>
+                                    );
+                                })}
                             </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                No results.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
+                        ))}
+                    </TableHeader>
+                    <TableBody className="bg-">
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell
+                                            key={cell.id}
+                                            style={{
+                                                ...getCommonPinningStyles({ column: cell.column }),
+                                            }}
+                                        >
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
 
-            {/* TODO: Add pagination controls */}
+            <DataTablePagination pagination={pagination} />
         </div>
     );
 }
