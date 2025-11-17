4fbbabc5d2eb69ffa3ef788cce1f36e2f1657322
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-17 06:11:55 -0300

    feat(layout): add PageLayout component for structured page layout with title and description

---
+++ resources/js/layouts/page/layout.tsx
@@ -0,0 +1,22 @@
+import Heading from '@/components/common/heading';
+import useIsMounted from '@/hooks/use-mounted';
+import { ReactNode, type PropsWithChildren } from 'react';
+
+interface PageLayoutProps extends PropsWithChildren {
+    title: string;
+    description: string;
+    children: ReactNode;
+}
+
+export default function PageLayout({ title, description, children }: Readonly<PageLayoutProps>) {
+    const isMounted = useIsMounted();
+    if (!isMounted) return null;
+
+    return (
+        <div className="px-4 py-6">
+            <Heading title={title} description={description} />
+
+            <section aria-label={`Section for ${title}`}>{children}</section>
+        </div>
+    );
+}
