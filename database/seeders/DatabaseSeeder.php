52d366bd54be953d615384ba8ccee79c00aa7f95
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-17 08:06:51 -0300

    feat(seeder): implement user role assignment logic in DatabaseSeeder for local environments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Enums\RoleEnum;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\App;
 
@@ -15,6 +17,14 @@ class DatabaseSeeder extends Seeder
         $this->call([PermissionSeeder::class, RoleSeeder::class, AdminSeeder::class]);
 
         if (App::environment('local', 'testing', 'staging')) {
+
+            // TODO: Extract this logic to a dedicated seeder class
+            $roleNames = array_values(array_filter(array_map(fn ($r) => $r->value, RoleEnum::cases()), fn ($r) => $r !== RoleEnum::SUPER_ADMIN->value));
+
+            User::factory()->count(100)->create()->each(function ($user) use ($roleNames) {
+                $user->syncRoles($roleNames[array_rand($roleNames)]);
+            });
+
             $this->call([]); // TODO: Add seeder classes for local, testing, and staging environments
         }
     }
