eeb3485b13969027b0286604d7937f12862fd858
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-18 07:50:15 -0300

    feat(column-visibility): add DataTableColumnVisibility component for managing column visibility

---
+++ resources/js/components/table/data-table-column-visibility.tsx
@@ -0,0 +1,49 @@
+'use no memo';
+
+import { Button } from '@/components/ui/button';
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu';
+import { normalizeString } from '@/lib/utils';
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import { Table } from '@tanstack/react-table';
+import { Settings2 } from 'lucide-react';
+
+export default function DataTableColumnVisibility<TData>({
+    table,
+}: Readonly<{
+    table: Table<TData>;
+}>) {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="sm" className="ml-auto">
+                    <Settings2 aria-hidden />
+                    View
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-[150px]">
+                <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {table
+                    .getAllColumns()
+                    .filter((column) => column.accessorFn !== undefined && column.getCanHide())
+                    .map((column) => {
+                        return (
+                            <DropdownMenuCheckboxItem
+                                key={column.id}
+                                checked={column.getIsVisible()}
+                                onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                            >
+                                {normalizeString(column.id)}
+                            </DropdownMenuCheckboxItem>
+                        );
+                    })}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
