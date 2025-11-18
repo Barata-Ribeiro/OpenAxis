e992fe31878155f2dfcc14f1a1269425a7a1a645
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-18 09:05:56 -0300

    feat(routes): add administrative routes for roles and users management

--- routes/administrative.php
+++ routes/administrative.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->prefix('administrative')->group(function () {
+    Route::prefix('roles')->group(function () {
+        Route::get('/', [RoleController::class, 'index'])->name('administrative.roles.index')->middleware('permission:role.index');
+    });
+
     Route::prefix('users')->group(function () {
         Route::get('/', [UserController::class, 'index'])->name('administrative.users.index')->middleware('permission:user.index');
     });
