7ff470b5417c7eef213c8310790ce40834c77ea4
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-19 09:07:38 -0300

    feat(users): add breadcrumbs and create route to Users page layout

--- resources/js/pages/administrative/users/index.tsx
+++ resources/js/pages/administrative/users/index.tsx
@@ -1,7 +1,8 @@
 import { DataTable } from '@/components/table/data-table';
 import AppLayout from '@/layouts/app-layout';
 import PageLayout from '@/layouts/page/layout';
-import { PaginationMeta } from '@/types';
+import administrative from '@/routes/administrative';
+import { BreadcrumbItem, PaginationMeta } from '@/types';
 import { UserWithRelations } from '@/types/application/user';
 import { Head } from '@inertiajs/react';
 import { columns } from './column';
@@ -10,18 +11,25 @@ interface IndexPageProps {
     users: PaginationMeta<UserWithRelations[]>;
 }
 
+const breadcrumbs: BreadcrumbItem[] = [{ title: 'Users', href: administrative.users.index().url }];
+
 export default function IndexPage({ users }: Readonly<IndexPageProps>) {
     const { data, ...pagination } = users;
 
     return (
-        <AppLayout>
+        <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="System Users" />
 
             <PageLayout
                 title="Users"
                 description="Listing all the registered users in the system, including the requesting user."
             >
-                <DataTable columns={columns} data={data} pagination={pagination} />
+                <DataTable
+                    columns={columns}
+                    data={data}
+                    pagination={pagination}
+                    createRoute={administrative.users.create()}
+                />
             </PageLayout>
         </AppLayout>
     );
