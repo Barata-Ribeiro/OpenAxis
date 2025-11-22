91274f02fb7c96619d09d312e310ded27cf8b780
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-22 09:28:53 -0300

    feat(profile): integrate AvatarUpload component and update User avatar type

--- resources/js/types/application/user.d.ts
+++ resources/js/types/application/user.d.ts
@@ -5,7 +5,10 @@ export interface User {
     id: number;
     name: string;
     email: string;
-    avatar?: string;
+    avatar?: {
+        original: string;
+        webp: string;
+    };
     email_verified_at: string | null;
     two_factor_enabled?: boolean;
     two_factor_confirmed_at: string | null;
