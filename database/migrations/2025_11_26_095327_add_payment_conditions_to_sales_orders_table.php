0be7cb67f19e4a294d52a5b270f8899bb4811a3c
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-26 09:59:53 -0300

    feat(migration): add payment_condition_id to sales_orders table with foreign key constraint

---
+++ database/migrations/2025_11_26_095327_add_payment_conditions_to_sales_orders_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sales_orders', function (Blueprint $table) {
+            $table->foreignId('payment_condition_id')->nullable()->constrained('payment_conditions')->cascadeOnUpdate()->nullOnDelete()->after('vendor_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sales_orders', function (Blueprint $table) {
+            $table->dropForeign(['payment_condition_id']);
+            $table->dropColumn('payment_condition_id');
+        });
+    }
+};
