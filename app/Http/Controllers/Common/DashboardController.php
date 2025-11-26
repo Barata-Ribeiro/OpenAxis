55f337debb0367e7bec4662dfc648097089f1ea8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-26 05:31:44 -0300

    feat(dashboard): enhance show method to validate yearMonth and fetch admin dashboard data

--- app/Http/Controllers/Common/DashboardController.php
+++ app/Http/Controllers/Common/DashboardController.php
@@ -4,23 +4,37 @@ namespace App\Http\Controllers\Common;
 
 use App\Enums\RoleEnum;
 use App\Http\Controllers\Controller;
+use App\Services\Common\DashboardService;
 use Auth;
+use Illuminate\Foundation\Http\FormRequest;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    public function show()
+    public function __construct(private DashboardService $dashboardService) {}
+
+    public function show(FormRequest $request)
     {
+        $validated = $request->validate([
+            'yearMonth' => ['sometimes', 'date_format:Y-m'],
+        ]);
+
+        $yearMonth = $validated['yearMonth'] ?? null;
+
         $userRole = Auth::user()->getRoleNames()->first();
         $data = [];
 
         switch ($userRole) {
             case RoleEnum::SUPER_ADMIN->value:
-                return Inertia::render('dashboards/admin-dashboard', $data);
+                $data = $this->dashboardService->getAdminDashboardData($yearMonth);
+
+                return Inertia::render('dashboards/admin-dashboard', ['data' => $data]);
             case RoleEnum::BUYER->value:
             case RoleEnum::VENDOR->value:
             case RoleEnum::FINANCE->value:
             default:
+                // $data = $this->dashboardService->getUserDashboardData(); TODO: Implement user dashboard data
+
                 return Inertia::render('dashboards/dashboard', $data);
         }
     }
