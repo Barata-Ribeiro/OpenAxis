577b993c9eade3d29704495bc504f47367c45b64
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-12-11 08:27:49 -0300

    feat(Inventory): create InventoryShowMovements component to display inventory movements in a data table

---
+++ resources/js/pages/erp/inventory/movements-columns.tsx
@@ -0,0 +1,97 @@
+import { DataTableColumnHeader } from '@/components/table/data-table-column-header';
+import { Badge } from '@/components/ui/badge';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import { InventoryMovementType, inventoryMovementTypeLabel } from '@/types/application/enums';
+import { StockMovement } from '@/types/erp/product';
+import { ColumnDef } from '@tanstack/react-table';
+import { format } from 'date-fns/format';
+import { CalendarIcon, CircleDashed } from 'lucide-react';
+
+export const columns: Array<ColumnDef<StockMovement>> = [
+    {
+        accessorKey: 'id',
+        header: ({ column }) => <DataTableColumnHeader column={column} title="ID" />,
+        enableSorting: true,
+        enableHiding: false,
+        size: 40,
+    },
+    {
+        accessorKey: 'movement_type',
+        header: ({ column }) => <DataTableColumnHeader column={column} title="Movement Type" />,
+        cell: function Cell({ row }) {
+            const rawMovementType = row.original.movement_type;
+
+            return (
+                <Badge className="select-none" variant="secondary">
+                    {inventoryMovementTypeLabel(rawMovementType)}
+                </Badge>
+            );
+        },
+        meta: {
+            label: 'Movement Type',
+            variant: 'multiSelect',
+            options: Object.values(InventoryMovementType).map((type) => ({
+                label: inventoryMovementTypeLabel(type),
+                value: type,
+            })),
+            icon: CircleDashed,
+        },
+        enableSorting: true,
+        enableColumnFilter: true,
+        enableHiding: false,
+    },
+    {
+        accessorKey: 'quantity',
+        header: ({ column }) => <DataTableColumnHeader column={column} title="Quantity" />,
+        cell: ({ row }) => (
+            <Badge
+                className="h-5 min-w-5 px-2 font-mono tabular-nums"
+                variant="outline"
+                aria-label={`${row.original.quantity} adjusted product(s)`}
+                title={`${row.original.quantity} adjustedproduct(s)`}
+            >
+                {row.original.quantity}
+            </Badge>
+        ),
+        enableSorting: true,
+    },
+    {
+        accessorKey: 'reason',
+        header: ({ column }) => <DataTableColumnHeader column={column} title="Reason" />,
+        cell: function Cell({ row }) {
+            return (
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <p className="max-w-[20ch] truncate">{row.original.reason}</p>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>{row.original.reason}</p>
+                    </TooltipContent>
+                </Tooltip>
+            );
+        },
+        enableSorting: false,
+    },
+    {
+        accessorKey: 'reference',
+        header: ({ column }) => <DataTableColumnHeader column={column} title="Reference" />,
+        enableSorting: false,
+    },
+    {
+        accessorKey: 'created_at',
+        header: ({ column }) => <DataTableColumnHeader column={column} title="Created At" />,
+        cell: ({ row }) => format(row.original.created_at, 'PPpp'),
+        meta: {
+            label: 'Created At',
+            variant: 'dateRange',
+            icon: CalendarIcon,
+        },
+        enableSorting: true,
+    },
+    {
+        accessorKey: 'updated_at',
+        header: ({ column }) => <DataTableColumnHeader column={column} title="Updated At" />,
+        cell: ({ row }) => format(row.original.updated_at, 'PPpp'),
+        enableSorting: true,
+    },
+];
---
+++ resources/js/pages/erp/inventory/show.tsx
@@ -0,0 +1,44 @@
+import { DataTable } from '@/components/table/data-table';
+import { usePermission } from '@/hooks/use-permission';
+import AppLayout from '@/layouts/app-layout';
+import PageLayout from '@/layouts/page/layout';
+import { columns } from '@/pages/erp/inventory/movements-columns';
+import erp from '@/routes/erp';
+import type { BreadcrumbItem, PaginationMeta } from '@/types';
+import type { Product, StockMovement } from '@/types/erp/product';
+import { Head } from '@inertiajs/react';
+
+interface InventoryShowMovementsProps {
+    product: Pick<Product, 'id' | 'name' | 'slug' | 'current_stock' | 'minimum_stock'>;
+    movements: PaginationMeta<Array<StockMovement>>;
+}
+
+export default function InventoryShowMovements({ product, movements }: Readonly<InventoryShowMovementsProps>) {
+    const { can } = usePermission();
+    const { data, ...pagination } = movements;
+
+    const breadcrumbs: BreadcrumbItem[] = [
+        { title: 'Inventory', href: erp.inventory.index().url },
+        { title: `#${product.name}`, href: erp.inventory.show(product.slug).url },
+    ];
+
+    // TODO: Display info about current stock and minimum stock
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title={`Inventory: ${product.name}`} />
+
+            <PageLayout
+                title={`#${product.name}`}
+                description={`Displaying all the inventory movements for "${product.name}".`}
+            >
+                <DataTable
+                    columns={columns}
+                    data={data}
+                    pagination={pagination}
+                    createRoute={can('supply.edit') ? erp.inventory.edit(product.slug) : undefined}
+                />
+            </PageLayout>
+        </AppLayout>
+    );
+}
