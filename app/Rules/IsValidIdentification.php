0858255264fade0895a9373ac129033871ae0460
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-12-17 08:23:24 -0300

    refactor(validation): improve type hinting and remove redundant function definitions in IsValidIdentification

--- app/Rules/IsValidIdentification.php
+++ app/Rules/IsValidIdentification.php
@@ -37,7 +37,7 @@ class IsValidIdentification implements ValidationRule
         }
     }
 
-    public function isValidCNPJ($cnpj)
+    public function isValidCNPJ(string $cnpj): bool
     {
         $cnpj = (string) $cnpj;
 
@@ -45,72 +45,65 @@ class IsValidIdentification implements ValidationRule
 
         $firstCnpjDigits = substr($cnpj, 0, 12);
 
-        if (! function_exists('multiplyCnpj')) {
+        $firstCalc = self::multiplyCnpj($firstCnpjDigits);
 
-            function multiplyCnpj($cnpj, $position = 5)
-            {
-                $calc = 0;
-
-                for ($i = 0; $i < strlen($cnpj); $i++) {
-                    $calc += $cnpj[$i] * $position;
-
-                    $position--;
-
-                    if ($position < 2) {
-                        $position = 9;
-                    }
-                }
-
-                return $calc;
-            }
-
-        }
-
-        $firstCalc = multiplyCnpj($firstCnpjDigits);
-
-        $firstDigit = ($firstCalc % 11) < 2 ? 0 : 11 - ($firstCalc % 11);
+        $firstDigit = $firstCalc % 11 < 2 ? 0 : 11 - $firstCalc % 11;
 
         $firstCnpjDigits .= $firstDigit;
 
-        $secondCalc = multiplyCnpj($firstCnpjDigits, 6);
+        $secondCalc = self::multiplyCnpj($firstCnpjDigits, 6);
 
-        $segundo_digito = ($secondCalc % 11) < 2 ? 0 : 11 - ($secondCalc % 11);
+        $segundo_digito = $secondCalc % 11 < 2 ? 0 : 11 - $secondCalc % 11;
 
-        $cnpj = $firstCnpjDigits.$segundo_digito;
+        $cnpj = "{$firstCnpjDigits}{$segundo_digito}";
 
         return $cnpj === $rawCnpj;
     }
 
-    public function isValidCPF($cpf = false)
+    public function isValidCPF(string $cpf): bool
     {
-        if (! function_exists('digitPositionsCalc')) {
-            function digitPositionsCalc($digits, $posicoes = 10, $digitSum = 0)
-            {
-
-                for ($i = 0; $i < \strlen($digits); $i++) {
-                    $digitSum += $digits[$i] * $posicoes;
-                    $posicoes--;
-                }
-
-                $digitSum %= 11;
-
-                $digitSum = ($digitSum < 2) ? 0 : 11 - $digitSum;
-
-                return "{$digits}{$digitSum}";
-            }
-        }
-
         $digits = substr($cpf, 0, 9);
 
-        $newCpf = digitPositionsCalc($digits);
+        $newCpf = self::digitPositionsCalc($digits);
 
-        $newCpf = digitPositionsCalc($newCpf, 11);
+        $newCpf = self::digitPositionsCalc($newCpf, 11);
 
         return $newCpf === $cpf;
     }
 
-    public function isValidSSN($ssn)
+    public function isValidSSN(string $ssn): bool
     {
         return (bool) preg_match('/^(?!000|666|9\d{2})\d{3}(?!00)\d{2}(?!0000)\d{4}$/', (string) $ssn);
+    }
+
+    private static function multiplyCnpj(string $cnpj, int $position = 5): int
+    {
+        $calc = 0;
+
+        for ($i = 0; $i < \strlen($cnpj); $i++) {
+            $calc += $cnpj[$i] * $position;
+
+            $position--;
+
+            if ($position < 2) {
+                $position = 9;
+            }
+        }
+
+        return $calc;
+    }
+
+    private static function digitPositionsCalc(string $digits, int $positions = 10, int $digitSum = 0): string
+    {
+        for ($i = 0; $i < \strlen($digits); $i++) {
+            $digitSum += $digits[$i] * $positions;
+            $positions--;
+        }
+
+        $digitSum %= 11;
+
+        $digitSum = ($digitSum < 2) ? 0 : 11 - $digitSum;
+
+        return "{$digits}{$digitSum}";
     }
 }
