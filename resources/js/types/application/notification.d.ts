4729fb432e5ee2428db2069dc0fd2f5357cd5ae4
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-12-24 07:16:43 -0300

    feat(notification): update Notification interface to include message in data and enhance type definitions

--- resources/js/types/application/notification.d.ts
+++ resources/js/types/application/notification.d.ts
@@ -1,9 +1,11 @@
+import type { InventoryMovementType } from '@/types/erp/erp-enums';
+
 export interface Notification<T = Record<string, unknown>> {
     id: string;
     type: string;
     notifiable_type: string;
     notifiable_id: number;
-    data: T;
+    data: { message: string } & T;
     read_at: string | null;
     created_at: string;
     updated_at: string;
@@ -13,4 +15,31 @@ export interface NotificationSummary<T = Record<string, unknown>> {
     latest: Notification<T>[];
     unread_count: number;
     total_count: number;
+}
+
+// Notifications Data
+export interface ManualSupplyAdjustmentData {
+    product: string;
+    movement_type: InventoryMovementType;
+    quantity: number;
+    reason: string | null;
+}
+
+export interface NewPurchaseOrderData {
+    purchase_order_id: number;
+    order_number: string;
+    order_date: string;
+    total_cost: number;
+    supplier: string;
+    created_by: string;
+}
+
+export interface NewSalesOrderData {
+    sales_order_id: number;
+    order_number: string;
+    order_date: string;
+    total_cost: number;
+    client: string;
+    vendor: string;
+    created_by: string;
 }
