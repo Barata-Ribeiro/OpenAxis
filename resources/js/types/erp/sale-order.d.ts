c3e11ba00074e1f593b8315d360c93e8149eae6a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-12-29 08:43:34 -0300

    feat(SaleOrderTypes): add SaleOrder and SaleOrderWithRelations interfaces for order management

---
+++ resources/js/types/erp/sale-order.d.ts
@@ -0,0 +1,36 @@
+import type { User } from '@/types/application/user';
+import type { SaleOrderStatus } from '@/types/erp/erp-enums';
+import type { Partner } from '@/types/erp/partner';
+import type { PaymentCondition } from '@/types/erp/payment-condition';
+import type { Vendor } from '@/types/erp/vendor';
+
+type PaymentMethod = 'cash' | 'credit_card' | 'debit_card' | 'bank_transfer';
+
+export interface SaleOrder {
+    id: number;
+    client_id: number;
+    vendor_id: number;
+    order_number: string;
+    order_date: string;
+    delivery_date: string;
+    status: SaleOrderStatus;
+    product_cost: number;
+    delivery_cost: number;
+    discount_cost: number;
+    total_cost: number;
+    product_value: number;
+    total_comission: number;
+    payment_method: PaymentMethod;
+    notes: string | null;
+    user_id: number;
+    created_at: string;
+    updated_at: string;
+    payment_condition_id: number;
+}
+
+export interface SaleOrderWithRelations extends SaleOrder {
+    client: Partner;
+    vendor: Vendor;
+    user: User;
+    payment_condition: PaymentCondition;
+}
