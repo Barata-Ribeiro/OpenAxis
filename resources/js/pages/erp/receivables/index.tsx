8ce9218232c058a3b9fe00519bafb4c9c2175a45
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-12-30 07:18:56 -0300

    feat(receivables): create IndexPage component for displaying receivables with pagination

---
+++ resources/js/pages/erp/receivables/index.tsx
@@ -0,0 +1,33 @@
+import { DataTable } from '@/components/table/data-table';
+import { usePermission } from '@/hooks/use-permission';
+import AppLayout from '@/layouts/app-layout';
+import PageLayout from '@/layouts/page/layout';
+import erp from '@/routes/erp';
+import type { BreadcrumbItem, PaginationMeta } from '@/types';
+import type { ReceivableWithRelations } from '@/types/erp/receivable';
+import { Head } from '@inertiajs/react';
+
+interface IndexPageProps {
+    receivables: PaginationMeta<ReceivableWithRelations[]>;
+}
+
+const breadcrumbs: BreadcrumbItem[] = [{ title: 'Receivables', href: erp.receivables.index().url }];
+
+export default function IndexPage({ receivables }: Readonly<IndexPageProps>) {
+    const { data, ...pagination } = receivables;
+    const { can } = usePermission();
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Receivables" />
+
+            <PageLayout title="Receivables" description="Listing all receivables in the system.">
+                <DataTable
+                    columns={[]} // TODO: Define columns for receivables
+                    data={data}
+                    pagination={pagination}
+                    createRoute={can('finance.create') ? erp.receivables.create() : undefined}
+                />
+            </PageLayout>
+        </AppLayout>
+    );
+}
