e3ba8d8b2127a627ac78b722dd73349f92c49b62
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-01-01 06:06:44 -0300

    feat(payables): add CreatePayablePage component for creating new payables

---
+++ resources/js/pages/erp/payables/create.tsx
@@ -0,0 +1,30 @@
+import NewPayableForm from '@/components/forms/payable/new-payable';
+import { Card, CardContent } from '@/components/ui/card';
+import AppLayout from '@/layouts/app-layout';
+import PageLayout from '@/layouts/page/layout';
+import erp from '@/routes/erp';
+import type { BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    { title: 'Payables', href: erp.payables.index().url },
+    { title: 'New Payable', href: erp.payables.create().url },
+];
+
+export default function CreatePayablePage() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Create Payable" />
+
+            <PageLayout title="Create Payable" description="Create a new payable in the system.">
+                <div className="mx-auto max-w-2xl">
+                    <Card>
+                        <CardContent>
+                            <NewPayableForm />
+                        </CardContent>
+                    </Card>
+                </div>
+            </PageLayout>
+        </AppLayout>
+    );
+}
