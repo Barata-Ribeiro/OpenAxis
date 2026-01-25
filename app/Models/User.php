c2c2f53cd85a6d64f563c512857fddc848d4fdeb
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-01-25 07:48:42 -0300

    feat(user): add method for notification broadcast channels

--- app/Models/User.php
+++ app/Models/User.php
@@ -158,4 +158,14 @@ class User extends Authenticatable implements Auditable, HasMedia, MustVerifyEma
     {
         return $this->addresses()->where('is_primary', true)->first();
     }
+
+    /**
+     * The channels the user receives notification broadcasts on.
+     *
+     * @return string
+     */
+    public function receiveBroadcastNotificationsOn()
+    {
+        return "App.Models.User.{$this->id}";
+    }
 }
