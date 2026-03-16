927f47fe0dc2de5bf444cabd2a8bd1cd4fa82f9d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-03-16 06:30:05 -0300

    refactor(vite): enhance Vite configuration with Babel integration and update JSX settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
@@ -11,17 +12,23 @@ export default defineConfig({
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
-        react({
-            babel: {
-                plugins: ['babel-plugin-react-compiler'],
-            },
-        }),
+        react(),
+        babel({ presets: [reactCompilerPreset()] }),
         tailwindcss(),
         wayfinder({
             formVariants: true,
         }),
     ],
-    esbuild: {
-        jsx: 'automatic',
+    oxc: {
+        jsx: {
+            runtime: 'automatic',
+            pure: true,
+            pragma: 'React.createElement',
+            pragmaFrag: 'React.Fragment',
+        },
+        typescript: {
+            jsxPragma: 'React.createElement',
+            jsxPragmaFrag: 'React.Fragment',
+        },
     },
 });
